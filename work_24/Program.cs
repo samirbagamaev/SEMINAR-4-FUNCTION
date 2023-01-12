df6571a6b2f3adee0965b9a64b4b45bc4e7e2add
Samir Bagamaev <05ru4315@gmail.com>
2023-01-12 20:48:33 +0300

    work_24

---
+++ work_24/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число (А) 
+// и выдаёт сумму чисел от 1 до А       (7 => 28) (4 =>10)
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetSum1ToA(int a)
+{
+   int sum = 0;
+   for(int i = 1; i <= a; i++)
+    {
+       sum += i; 
+       //sum = sum + i
+    }
+   return sum;
+}
+
+int number = ReadNumber("Введите чиcло: ");
+if (number > 0)
+{
+    int sum = GetSum1ToA(number);
+    Console.WriteLine(sum);
+}
+else 
+{
+Console.WriteLine("Число не правильное");
+}
